02828824580c86a024d3230cef403232360c5750
Sidharth Arya <sidhartharya10@gmail.com>
2018-11-05 07:34:53 +0530

    Initials

--- essentials.el
+++ essentials.el
@@ -16,8 +16,7 @@
     (newline)
     )
   )
-(defvar font-size 150)
-  (set-face-attribute 'default nil :height  font-size)
+(defvar font-size 100)
 (defun increase-font-size()
   "Increase the font Size"
   (interactive)
@@ -35,13 +34,37 @@
       (global-whitespace-mode 0)
     (global-whitespace-mode 1))
   )
-  
-(defun find-from-dict(list option)
+
+(defun find-from-dict(list option &optional elem)
   "Allows to use list of list as a dictionary."
-  (nth 1 (nth (position option list :test (lambda (a b) (member a b))) list))
+  (if (equal elem nil)
+      (nth 1 (nth (cl-position option list :test (lambda (a b) (member a b))) list))
+    (nth elem (nth (cl-position option list :test (lambda (a b) (member a b))) list))
+    )
   )
-    
-    
+
+(defun insert-into-string(string identifier repl)
+  "replace an identifier in a string"
+  (string-join(split-string string identifier) repl)
+  )
+(defun set-fringemark-at-point(&optional Point)
+  "Set Mark on Fringe"
+  (interactive)
+  (let ((marker (make-marker)))
+    (save-excursion
+      (if (not (equal Point nil))
+	  (goto-line Point))
+     (forward-line 0)
+   (set-marker marker (point))
+  (setq overlay-arrow-cl-position marker)
+    )))
+(defun smartpairs-enable()
+  "Enable smart matching parenthesis"
+  (define-key global-map (kbd "{") 'skeleton-pair-insert-maybe)
+  (define-key global-map (kbd "[") 'skeleton-pair-insert-maybe)
+  (define-key global-map (kbd "(") 'skeleton-pair-insert-maybe)
+  (define-key global-map (kbd "\"") 'skeleton-pair-insert-maybe)
+  )
     
 ;; Kill Ring
 
